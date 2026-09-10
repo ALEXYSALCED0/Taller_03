109604c96dd39cdf9d7e6848a45764fd2e96c5da
Alexy De Jesus Salcedo Tete <alexysalcedo01@gmail.com>
2026-09-10 09:34:08 -0500

    feat: añadir boton

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css'
+function Button({children, onClick, variant='primary'}){
+    return(
+        <button className={`button button--${variant}`} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+export default Button
