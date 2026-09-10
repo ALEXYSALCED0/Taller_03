b3e90b0dc73d237b16bd93a1a69f42f9065b1f18
Alexy De Jesus Salcedo Tete <alexysalcedo01@gmail.com>
2026-09-10 17:05:21 -0500

    style: tamaño de fuente y peso  en variante de boton redonda

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import CoursesSection from "./components/CoursesSection"
+import RegistrationSection from './components/RegistrationSection'
 function App() {
   return (
   <>
     <Header> </Header>
     <HeroSection> </HeroSection>
     <CoursesSection></CoursesSection>
+    <RegistrationSection> </RegistrationSection>
   </>
   )
 }
