c9435cc21193a9abd41d1351eff3dc6156cefb9d
Alexy De Jesus Salcedo Tete <alexysalcedo01@gmail.com>
2026-09-10 15:42:55 -0500

    feat: add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
+import CoursesSection from "./components/CoursesSection"
 function App() {
   return (
   <>
     <Header> </Header>
+    <HeroSection> </HeroSection>
+    <CoursesSection></CoursesSection>
   </>
   )
 }
